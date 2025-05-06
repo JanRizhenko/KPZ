0c20c2cffd28d27afda391109da33229d38e6bc1
JanRizhenko <ipz231_ryav@student.ztu.edu.ua>
2025-05-06 15:04:11 +0300

    task 4 is done

---
+++ Lab03/Composite + FlyWeight/Composite/ImageLoading.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class LocalFileLoadingStrategy : IImageLoadingStrategy
+    {
+        public void Load(string path)
+        {
+            Console.WriteLine($"Loading image from local file: {path}");
+        }
+    }
+
+    public class NetworkLoadingStrategy : IImageLoadingStrategy
+    {
+        public void Load(string path)
+        {
+            Console.WriteLine($"Downloading image from web: {path}");
+        }
+    }
+}
---
+++ Lab03/Composite + FlyWeight/Composite/LightImageNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class LightImageNode : LightNode
+    {
+        public string _src;
+        private IImageLoadingStrategy _strategy;
+
+        public LightImageNode(string src)
+        {
+            _src = src;
+            _strategy = ChooseStrategy(src);
+        }
+
+        private IImageLoadingStrategy ChooseStrategy(string src)
+        {
+            return (src.StartsWith("http://") || src.StartsWith("https://"))
+                ? (IImageLoadingStrategy)new NetworkLoadingStrategy()
+                : new LocalFileLoadingStrategy();
+        }
+
+        public void LoadImage()
+        {
+            _strategy.Load(_src);
+        }
+
+        public override string InnerHTML => "";
+        public override string OuterHTML => $"<img src=\"{_src}\" />";
+    }
+}
